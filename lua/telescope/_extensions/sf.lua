6c4b063658f337ac3e4f52593ac2d95ddfff7336
kwhite <knwdevs@gmail.com>
2024-03-02 17:39:02 +0000

    Revert "Update README.md"

--- lua/telescope/_extensions/sf.lua
+++
@@ -1,8 +0,0 @@
-return require("telescope").register_extension({
-	setup = function(ext_config, config)
-		-- access extension config and user config
-	end,
-	exports = {
-		custom_objects = require("sf.org").search_custom_objects(),
-	},
-})
